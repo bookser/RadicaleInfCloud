79329ea0188c70a4ff4734bf65cf4bbefe99c5f2
bookser <bookser@live.com>
2023-06-12 15:56:15 +0800

    Update __init__.py

--- radicale_infcloud/__init__.py
+++ radicale_infcloud/__init__.py
@@ -1,5 +1,5 @@
 # RadicaleWeb web interface for Radicale.
-# Copyright (C) 2017 Unrud <unrud@openaliasbox.org>
+# Copyright © 2017-2018, 2020, 2022 Unrud <unrud@outlook.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,59 +14,36 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
-import pkg_resources
-import posixpath
-import time
-
 from http import client
-from radicale import storage, web
-from radicale.web import NOT_FOUND, MIMETYPES, FALLBACK_MIMETYPE
+from radicale import httputils
+from radicale.web import internal
 
-VERSION = "2.0.0"
+PLUGIN_CONFIG_SCHEMA = {"web": {
+    "infcloud_config": {
+        "value": "",
+        "type": str}}}
 
 
-class Web(web.Web):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.infcloud_folder = pkg_resources.resource_filename(__name__, "web")
+class Web(internal.Web):
+
+    def __init__(self, configuration):
+        super().__init__(configuration.copy(PLUGIN_CONFIG_SCHEMA))
 
     def get(self, environ, base_prefix, path, user):
-        if not path.startswith("/.web/infcloud/") and path != "/.web/infcloud":
-            status, headers, answer = super().get(environ, base_prefix, path,
-                                                  user)
-            if status == client.OK and path in ("/.web/", "/.web/index.html"):
-                answer = answer.replace(b"""\
-        <nav>
-            <ul>""", b"""\
-        <nav>
-            <ul>
-                <li><a href="infcloud">InfCloud</a></li>""")
-            return status, headers, answer
-        try:
-            filesystem_path = storage.path_to_filesystem(
-                self.infcloud_folder, path[len("/.web/infcloud"):])
-        except ValueError as e:
-            self.logger.debug("Web content with unsafe path %r requested: %s",
-                              path, e, exc_info=True)
-            return NOT_FOUND
-        if os.path.isdir(filesystem_path) and not path.endswith("/"):
-            location = posixpath.basename(path) + "/"
-            return (client.SEE_OTHER,
-                    {"Location": location, "Content-Type": "text/plain"},
-                    "Redirected to %s" % location)
-        if os.path.isdir(filesystem_path):
-            filesystem_path = os.path.join(filesystem_path, "index.html")
-        if not os.path.isfile(filesystem_path):
-            return NOT_FOUND
-        content_type = MIMETYPES.get(
-            os.path.splitext(filesystem_path)[1].lower(), FALLBACK_MIMETYPE)
-        with open(filesystem_path, "rb") as f:
-            answer = f.read()
-            last_modified = time.strftime(
-                "%a, %d %b %Y %H:%M:%S GMT",
-                time.gmtime(os.fstat(f.fileno()).st_mtime))
-        headers = {
-            "Content-Type": content_type,
-            "Last-Modified": last_modified}
-        return client.OK, headers, answer
+        if path == "/.web/infcloud/" or path.startswith("/.web/infcloud"):
+            status, headers, answer = httputils.serve_resource(
+                "radicale_infcloud", "web", base_prefix, path,
+                "/.web/infcloud")
+        else:
+            status, headers, answer = super().get(
+                environ, base_prefix, path, user)
+        if status == client.OK and path in ("/.web/", "/.web/index.html"):
+            answer = answer.replace(b"""\
+<nav>
+    <ul>""", b"""\
+<nav>
+    <ul>
+        <li><a href="infcloud">InfCloud</a></li>""")
+        if status == client.OK and path == "/.web/infcloud/config.js":
+            answer += self.configuration.get("web", "infcloud_config").encode()
+        return status, headers, answer
